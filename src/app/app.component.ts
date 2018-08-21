9e34bc218044123771feaa6202d37794130bb598
unknown <pedrohofernandes@gmail.com>
2018-08-21 20:19:35 -0300

    #36

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,9 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
-      this.splashScreen.hide();    
+      setTimeout(() => {
+        this.splashScreen.hide();
+      }, 100);
     });
   }
   
